d0e6eea77963e54471c159c16b637372262d9e75
Jaeven Tanuwidjaja <93062841+jaevent24@users.noreply.github.com>
2024-02-22 01:07:50 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-function alertbutton() {
-    alert("Hello World!")
-}
+function downloadPDF() {
+    
+    const pdfUrl = 'Resume.pdf';
+    
+    const downloadLink = document.createElement('a');
+    
+    downloadLink.href = pdfUrl;
+    
+    downloadLink.download = 'JaevenResume.pdf';
+    
+    document.body.appendChild(downloadLink);
+    
+    downloadLink.click();
+    
+    document.body.removeChild(downloadLink);
+  }
